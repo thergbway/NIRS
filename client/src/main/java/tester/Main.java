1c7fc3595be65ce9d79c8d148d8a1e7fce469956
Dmitriy Korobov <ZeroPointSystem@gmail.com>
2016-05-23 22:14:46 +0300

    Login wallpaper added

---
+++ client/src/main/java/tester/Main.java
@@ -0,0 +1,18 @@
+//package tester;
+//
+//import nirs.api.MainService;
+//import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+//
+//import java.io.FileInputStream;
+//import java.io.IOException;
+//
+//public class Main {
+//    public static void main(String[] args) throws IOException {
+//        AnnotationConfigApplicationContext cx = new AnnotationConfigApplicationContext(Config.class);
+//
+//        MainService service = cx.getBean("mainService", MainService.class);
+//
+//        System.out.println(service.getGreeting("some guy"));
+//        System.out.println(service.uploadToMongo("sdf.txt", new FileInputStream("C:\\Users\\AND\\Desktop\\AngularJS для новичка- вчера, сегодня, завтра.mp4")));
+//    }
+//}
