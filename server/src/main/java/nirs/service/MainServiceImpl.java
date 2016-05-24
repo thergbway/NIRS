fa3a1bf8d2c868dbeec930a47eaf32319f66fbfb
thergbway <thergbworld@gmail.com>
2016-05-24 11:27:13 +0300

    Renamed one func to downloadFilePart

--- server/src/main/java/nirs/service/MainServiceImpl.java
+++ server/src/main/java/nirs/service/MainServiceImpl.java
@@ -10,6 +10,7 @@ import nirs.api.model.FileInfo;
 import nirs.api.model.UserInfo;
 import nirs.dao.FilesService;
 import nirs.dao.UserService;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +19,7 @@ import java.util.List;
 
 @Service
 public class MainServiceImpl implements MainService {
+    private static final Logger logger = Logger.getLogger(MainServiceImpl.class);
 
     @Autowired
     private UserService userService;
@@ -27,71 +29,114 @@ public class MainServiceImpl implements MainService {
 
     @Override
     public void addNewUser(String username, String password, String firstName, String lastName, String email) throws UserExistsException, EmailExistsException {
-        if(userService.isUsernamePresented(username))
+        logger.debug(String.format("addNewUser() executing. Params: username = %1s, password = ?, firstName = %2s, " +
+            "lastName = %3s, email = %4s", username, firstName, lastName, email));
+        if (userService.isUsernamePresented(username)) {
+            logger.debug(".....username exists exception");
             throw new UserExistsException(username);
-        if(userService.isEmailPresented(email))
+        }
+        if (userService.isEmailPresented(email)) {
+            logger.debug(".....email exists exception");
             throw new EmailExistsException(email);
+        }
 
         userService.createUser(username, password, firstName, lastName, email);
+        logger.debug(".....success");
     }
 
     @Override
     public String getToken(String username, String password) throws InvalidCredentialsException {
-        if (!userService.isValidCredentials(username, password))
+        logger.debug(String.format("getToken() executing. Params: username = %1s, password = ?", username));
+        if (!userService.isValidCredentials(username, password)) {
+            logger.debug(".....invalid credentials exception");
             throw new InvalidCredentialsException();
+        }
 
-        return userService.createToken(username);
+        String token = userService.createToken(username);
+        logger.debug(".....success");
+        return token;
     }
 
     @Override
     public UserInfo getUserInfo(String token) throws InvalidTokenException {
-        if (!userService.isValidToken(token))
+        logger.debug(String.format("getUserInfo() executing. Params: token = %1s", token));
+        if (!userService.isValidToken(token)) {
+            logger.debug(".....invalid token exception");
             throw new InvalidTokenException();
+        }
 
-        return userService.getUserInfo(token);
+        UserInfo userInfo = userService.getUserInfo(token);
+        logger.debug(".....success");
+        return userInfo;
     }
 
     @Override
     public List<FileInfo> getFiles(String token) throws InvalidTokenException {
-        if (!userService.isValidToken(token))
+        logger.debug(String.format("getFiles() executing. Params: token = %1s", token));
+        if (!userService.isValidToken(token)) {
+            logger.debug(".....invalid token exception");
             throw new InvalidTokenException();
+        }
 
         String username = userService.getUsername(token);
 
-        return filesService.getFiles(username);
+        List<FileInfo> files = filesService.getFiles(username);
+        logger.debug(String.format(".....success. Files count = %1d", files.size()));
+        return files;
     }
 
     @Override
     public FileInfo getFile(String token, String id) throws InvalidTokenException {
-        if (!userService.isValidToken(token))
+        logger.debug(String.format("getFile() executing. Params: token = %1s, id = %2s", token, id));
+        if (!userService.isValidToken(token)) {
+            logger.debug(".....invalid token exception");
             throw new InvalidTokenException();
+        }
 
-        return filesService.getFile(id);
+        FileInfo file = filesService.getFile(id);
+        logger.debug(".....success");
+        return file;
     }
 
     @Override
     public void deleteFile(String token, String id) throws InvalidTokenException {
-        if (!userService.isValidToken(token))
+        logger.debug(String.format("deleteFile() executing. Params: token = %1s, id = %2s", token, id));
+        if (!userService.isValidToken(token)) {
+            logger.debug(".....invalid token exception");
             throw new InvalidTokenException();
+        }
 
         filesService.deleteFile(id);
+        logger.debug(".....success");
     }
 
     @Override
     public byte[] downloadFilePart(String token, String id, int offset) throws InvalidTokenException {
-        if (!userService.isValidToken(token))
+        logger.debug(String.format("downloadFilePart() executing. Params: token = %1s, id = %2s, offset = %3s",
+            token, id, offset));
+        if (!userService.isValidToken(token)) {
+            logger.debug(".....invalid token exception");
             throw new InvalidTokenException();
+        }
 
-        return filesService.getFilePart(id, offset);
+        byte[] filePart = filesService.getFilePart(id, offset);
+        logger.debug(String.format(".....success. Returning %1.3f kB", (double) filePart.length / 1024.0));
+        return filePart;
     }
 
     @Override
     public FileInfo uploadFile(String token, String filename, Cipher cipher, InputStream in) throws InvalidTokenException {
-        if (!userService.isValidToken(token))
+        logger.debug(String.format("uploadFile() executing. " +
+            "Params: token = %1s, filename = %2s, cipher = %3s", token, filename, cipher));
+        if (!userService.isValidToken(token)) {
+            logger.debug(".....invalid token exception");
             throw new InvalidTokenException();
+        }
 
         String username = userService.getUsername(token);
 
-        return filesService.uploadFile(username, filename, cipher, in);
+        FileInfo fileInfo = filesService.uploadFile(username, filename, cipher, in);
+        logger.debug(".....success");
+        return fileInfo;
     }
 }
